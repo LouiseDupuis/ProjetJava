5b8e7d98178a53631ff158b5c345a1f61ca89f63
ArianeDlns <ariane.dalens@student.ecp.fr>
2018-11-10 13:14:38 +0100

    Commit 2
    Test

---
+++ Group7_IS1220_Project_part1_Dalens_Dupuis/src/myUberCore/GPS.java
@@ -0,0 +1,32 @@
+package myUberCore;
+
+public class GPS {
+	
+	private double latitude ;
+	private double longitude ;
+	
+	public GPS(double latitude, double longitude) {
+		super();
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+	
+	
+
+}
---
+++ Group7_IS1220_Project_part1_Dalens_Dupuis/src/myUberCore/Message.java
@@ -0,0 +1,5 @@
+package myUberCore;
+
+public class Message {
+
+}
